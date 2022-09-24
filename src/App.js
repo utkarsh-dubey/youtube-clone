c46dd99cc2582af3ad68be3858bccb95eedee0d2
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2022-09-24 14:07:29 +0530

    day1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import { Box } from '@mui/material';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Video from './Components/Video';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Box sx={{backgroundColor:"#000"}}>
+        <Navbar/>
+         <Home/>
+      </Box>
+      
     </div>
   );
 }
